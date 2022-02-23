377e6a386b21f54003930696be37c627bd87020c
mayardes <mayardesoliveira@gmail.com>
2022-02-23 14:22:37 -0400

    create layers - create application - create domain - create infra - create service - create Test

---
+++ FINANCE.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FINANCE.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ FINANCE.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FINANCE.Service
+{
+    public class Class1
+    {
+    }
+}
